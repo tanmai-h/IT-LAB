e889a1b25ab12965484cd8c2e723035d9f139bc9
Tanmai H <vvijaytanmai@gmail.com>
2019-02-27 12:13:06 +0530

    os

--- sem4/os/buffer.java
+++ sem4/os/buffer.java
@@ -1,14 +1,9 @@
-// Java program to implement solution of producer 
-// consumer problem. 
 import java.util.LinkedList; 
 
 public class buffer 
 { 
 	public static void main(String[] args) 
-						throws InterruptedException 
-	{ 
-		// Object of a class that has both produce() 
-		// and consume() methods 
+						throws InterruptedException 	{ 
 		final PC pc = new PC(); 
 
 		// Create producer thread 
@@ -54,25 +49,17 @@ public class buffer
 		t2.join(); 
 	} 
 
-	// This class has a list, producer (adds items to list 
-	// and consumber (removes items). 
-	public static class PC 
-	{ 
-		// Create a list shared by producer and consumer 
-		// Size of list is 2. 
+	public static class PC 	{ 
 		LinkedList<Integer> list = new LinkedList<>(); 
 		int capacity = 2; 
 
-		// Function called by producer thread 
 		public void produce() throws InterruptedException 
 		{ 
 			int value = 0; 
 			while (true) 
 			{ 
 				synchronized (this) 
-				{ 
-					// producer thread waits while list 
-					// is full 
+				{  
 					while (list.size()==capacity) 
 						wait(); 
 
@@ -82,12 +69,8 @@ public class buffer
 					// to insert the jobs in the list 
 					list.add(value++); 
 
-					// notifies the consumer thread that 
-					// now it can start consuming 
 					notify(); 
 
-					// makes the working of program easier 
-					// to understand 
 					Thread.sleep(1000); 
 				} 
 			} 
