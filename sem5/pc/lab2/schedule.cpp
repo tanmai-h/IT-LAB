deaefd3c2e096aa960759de0e3e1d517cd37dd3c
tanmai-h <vvijaytanmai@gmail.com>
2019-08-21 19:39:24 +0530

    Update schedule.cpp

--- sem5/pc/lab2/schedule.cpp
+++ sem5/pc/lab2/schedule.cpp
@@ -3,97 +3,54 @@
 
 using namespace std;
 
-int prime_default ( int n) {
-    int i;
-    int j;
-    int prime;
+int default_squares ( int n) {
+    int i,j;
     int total = 0;
 
-    #pragma omp parallel shared (n) private (i,j,prime)
-
+    #pragma omp parallel shared (n) private (i,j)
     #pragma omp for reduction (+:total)
-    for ( i = 2; i <= n; i++ ) {
-        prime = 1;
-        for ( j = 2; j < i; j++ ) {
-            if ( i % j == 0 ) {
-                prime = 0;
-                break;
-            }
-        }
-        total = total + prime;
-    }
+    for ( i = 1; i <= n; i++ )
+        if(i == (int)sqrt(i) * (int)sqrt(i))
+            total += 1; 
 
     return total;
 }
 
-int prime_static ( int n ) {
-    int i;
-    int j;
-    int prime;
+int static_squares ( int n ) {
+    int i,j;
     int total = 0;
 
-    # pragma omp parallel shared ( n ) private ( i, j, prime )
-
+    #pragma omp parallel shared (n) private (i,j)
     # pragma omp for reduction ( + : total ) schedule ( static, 100 )
-    for ( i = 2; i <= n; i++ ) {
-        prime = 1;
-
-        for ( j = 2; j < i; j++ ) {
-            if ( i % j == 0 ) {
-                prime = 0;
-                break;
-            }
-        }
-        total = total + prime;
-    }
+    for ( i = 1; i <= n; i++ )
+        if(i == (int)sqrt(i) * (int)sqrt(i))
+            total += 1;
 
     return total;
 }
 
-int prime_dynamic ( int n ) {
-    int i;
-    int j;
-    int prime;
+int dynamic_squares ( int n ) {
+    int i,j;
     int total = 0;
 
-    #pragma omp parallel shared (n) private (i,j,prime)
-
+    #pragma omp parallel shared (n) private (i,j)
     #pragma omp for reduction (+:total) schedule (dynamic,100)
-    for ( i = 2; i <= n; i++ ) {
-        prime = 1;
-
-        for ( j = 2; j < i; j++ ) {
-            if ( i % j == 0 ) {
-                prime = 0;
-                break;
-            }
-        }
-        total = total + prime;
-    }
+    for ( i = 1; i <= n; i++ ) 
+        if(i == (int)sqrt(i) * (int)sqrt(i))
+            total += 1;
 
     return total;
 }
 
-int prime_guided ( int n ) {
-    int i;
-    int j;
-    int prime;
+int guided_squares ( int n ) {
+    int i,j;
     int total = 0;
 
-    #pragma omp parallel shared (n) private (i,j,prime)
-
+    #pragma omp parallel shared (n) private (i,j)
     #pragma omp for reduction (+:total) schedule (guided,100)
-    for ( i = 2; i <= n; i++ ) {
-        prime = 1;
-
-        for ( j = 2; j < i; j++ ) {
-            if ( i % j == 0 ) {
-                prime = 0;
-                break;
-            }
-        }
-        total = total + prime;
-    }
+    for ( i = 1; i <= n; i++ )
+        if(i == (int)sqrt(i) * (int)sqrt(i))
+            total += 1;
 
     return total;
 }
@@ -101,32 +58,32 @@ int prime_guided ( int n ) {
 int main ( int argc, char *argv[] ){
     omp_set_num_threads(8);
     int n, low = 1, high = 265000;
-    int primes;
+    int squares;
     double times[4]; 
 
     printf ( "Loop Schedule\n" );
     printf ( "                           Default        Static       Dynamic       Guided\n" );
-    printf ( "         N     num Primes Till N\n");
+    printf ( "         N     num Squares Till N\n");
 
     n = low;
     while (n <= high) {
         times[0] = omp_get_wtime ();
-        primes = prime_default (n);
+        squares = default_squares (n);
         times[0] = omp_get_wtime () - times[0];
 
         times[1] = omp_get_wtime ();
-        primes = prime_static (n);
+        squares = static_squares (n);
         times[1] = omp_get_wtime () - times[1];
 
         times[2] = omp_get_wtime ();
-        primes = prime_dynamic (n);
+        squares = dynamic_squares (n);
         times[2] = omp_get_wtime () - times[2];
 
         times[3] = omp_get_wtime ();
-        primes = prime_guided (n);
+        squares = guided_squares (n);
         times[3] = omp_get_wtime () - times[3];
 
-        printf ( "  %8d  %8d  %12f  %12f  %12f %12f\n", n, primes, times[0], times[1], times[2], times[3] );
+        printf ( "  %8d  %8d  %12f  %12f  %12f %12f\n", n, squares, times[0], times[1], times[2], times[3] );
 
         n = n * 2;
     }
