9a9c81f85b1cab1ebd4f3beea774181deaff4376
tanmai-h <vvijaytanmai@gmail.com>
2019-08-22 16:50:24 +0530

    acd lab 1

---
+++ sem5/acd/lab1/code.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc[],char **argv[]) {
+    int x, *9;
+    int a; int b; int *&*&&;
+
+    /* sdsdsdsd */
+    // sds098090349034903
+    //sd
+    printf("sfsd\n");
+    scanf("%d", &x);
+    printf("sfsd\n");
+    scanf("%d", &x);
+    printf("sfsd\n");
+    printf("sfsd\n");
+    printf("sfsd\n");
+    scanf("%d", &x);
+
+    return 0; 
+}
---
+++ sem5/acd/lab1/newcode.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc[],char **argv[]) {
+    int x, *9;
+    int a; int b; int *&*&&;
+
+     
+     
+     
+    printf("sfsd\n");
+    scanf("%d", &x);
+    printf("sfsd\n");
+    scanf("%d", &x);
+    printf("sfsd\n");
+    printf("sfsd\n");
+    printf("sfsd\n");
+    scanf("%d", &x);
+
+    return 0; 
+}
