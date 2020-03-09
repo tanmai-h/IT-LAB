b5c3f279e870735a9385bb2d6547bc69c0e1f642
tanmai-h <vvijaytanmai@gmail.com>
2020-03-09 14:48:54 +0530

    dm

--- sem6/dm/colossal/colossal.py
+++ sem6/dm/colossal/colossal.py
@@ -4,7 +4,7 @@ import numpy as np
 cardinality_count = 2
 support_count = 2
 
-def load_data(fname='Test.csv'):
+def GetData(fname='Test.csv'):
 	dataset = []
 
 	with open(fname, 'r') as file:
@@ -15,16 +15,17 @@ def load_data(fname='Test.csv'):
 
 	return np.array(dataset).astype(int)
 
-def bitmap_intersection(l1, l2):	
+def Intersection(l1, l2):	
 	return 1 * np.logical_and(l1, l2)
 
-def check_empty(row, cardinality):
+def CheckEmpty(row, cardinality):
 	return np.sum(row) >= cardinality
 
-def min_support_check(dataset, new_itemset):
+def CheckMinSup(dataset, new_itemset):
 	sup_count = 0
 
 	tmp = 1 * np.logical_and(dataset, new_itemset)
+
 	for row in tmp:
 		if np.sum(row) == np.sum(new_itemset):	
 			sup_count += 1
@@ -48,8 +49,8 @@ def convert_to_itemsets(dataset,bitvector):
 
 	return np.array(itemset)
 
-def row_pruning(dataset, val):
-	ls, nm =[], []
+def PruneRow(dataset, val):
+	ls, nm = [], []
 	
 	for i in range(len(dataset)):
 		if np.sum(dataset[i]) >= val:
@@ -58,37 +59,37 @@ def row_pruning(dataset, val):
 
 	return np.array(ls), np.array(nm)
 
-def column_pruning(dataset):
+def PruneCol(dataset):
 	new = dataset.T
 	
-	final, nm = row_pruning(new,support_count)
+	final, nm = PruneRow(new,support_count)
 
 	return final.T, nm
 
-def create_dict(nm):
+def MakeDict(nm):
 	dct = {}
 	for i,j in enumerate(nm):
 		dct[i] = j
 
 	return dct
 
-def compare_dict(dct,final):
+def CompareDict(dct,final):
 	for i in range(len(final)):
 		for j in range(len(final[i])):
 			final[i][j]=dct[final[i][j]]
 
 	return np.array(final)
 
-def row_enumerate(dataset):
+def EnumerateRow(dataset):
 	queue = []
 	final = []
 	for i in range(len(dataset)):
 		queue.append(([i],dataset[i]))
 	
 	for i in range(len(queue)):
-		x, support = min_support_check(dataset,queue[i][1])
+		x, support = CheckMinSup(dataset,queue[i][1])
 
-		if(check_empty(queue[i][1], cardinality_count) and support>= support_count):
+		if(CheckEmpty(queue[i][1], cardinality_count) and support >= support_count):
 			final.append(convert_to_itemsets(dataset,queue[i][1]))
 
 	while(len(queue)!=0):
@@ -98,32 +99,35 @@ def row_enumerate(dataset):
 		for i in range(last_val+1,len(dataset)):
 			new_list = rows.copy()
 			new_list.append(i)
-			new_intersect = bitmap_intersection(intersection,dataset[i])
-			x, support = min_support_check(dataset,new_intersect)
+			new_intersect = Intersection(intersection,dataset[i])
+			x, support = CheckMinSup(dataset,new_intersect)
 
-			if(check_empty(new_intersect,cardinality_count) and support>= support_count):
+			if(CheckEmpty(new_intersect,cardinality_count) and support>= support_count):
 				queue.append((new_list,new_intersect))
+
 				final.append(convert_to_itemsets(dataset,new_intersect))
 
 	return final
 
 if __name__ == '__main__':
-	dataset = load_data()
-	print(dataset)
-	dataset, x = row_pruning(dataset,cardinality_count)
-	dataset, nm = column_pruning(dataset)
-	dct = create_dict(nm)
-	
-	final = row_enumerate(dataset)
-	
-	final = compare_dict(dct,final)
-	final = np.array(sorted(final, key = len))
-	
-	res = [] 
-	
-	for x in final:
-		if list(x) not in res:
-			res.append(list(x))
+    dataset = GetData()
+    print(dataset)
 
-	print(res)
-	
+    dataset, x = PruneRow(dataset,cardinality_count)
+    dataset, nm = PruneCol(dataset)
+    print(dataset)
+    dct = MakeDict(nm)
+
+    final = EnumerateRow(dataset)
+
+    final = CompareDict(dct,final)
+
+    final = np.array(sorted(final, key = len))
+
+    res = [] 
+
+    for x in final:
+        if list(x) not in res:
+            res.append(list(x))
+
+    print(res`)
